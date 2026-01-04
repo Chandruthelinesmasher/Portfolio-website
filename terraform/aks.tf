fc365a590e523b1bbb97a91f183e63ed598ce57d
chandru <kumaravelchandru982@gmail.com>
2026-01-04 09:40:18 +0530

    test: trigger workflow with verified credentials

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name                = "default"
     node_count          = 1
-    vm_size             = "Standard_DC2s_v3"
+    vm_size             = "Standard_B2s"
     type                = "VirtualMachineScaleSets"
     vnet_subnet_id      = azurerm_subnet.private[0].id
   }
